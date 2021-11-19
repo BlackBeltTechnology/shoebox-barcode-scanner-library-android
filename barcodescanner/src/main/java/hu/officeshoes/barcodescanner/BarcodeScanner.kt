bfb5255ea689504f9781e3dc50eff11c361fe45b
Geczy Zsolt <zsolt.geczy@blackbelt.hu>
2021-11-19 19:04:58 +0100

    Increment version number

--- barcodescanner/src/main/java/hu/officeshoes/barcodescanner/BarcodeScanner.kt
+++ barcodescanner/src/main/java/hu/officeshoes/barcodescanner/BarcodeScanner.kt
@@ -15,8 +15,8 @@ import hu.officeshoes.barcodescanner.scanner.handler.SingleBarcodeScannerListene
 
 object BarcodeScanner {
 
-    const val VERSION_CODE = 3
-    const val VERSION_NAME = "1.0.2"
+    const val VERSION_CODE = 4
+    const val VERSION_NAME = "1.1.0"
 
     private var barcodeScannerListenerHandler: BaseBarcodeScannerListenerHandler? = null
 
